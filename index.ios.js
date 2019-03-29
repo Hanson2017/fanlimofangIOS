7f00d813a0c6fcb1969680a8be7771cd14726fe0
Hanson2017 <18818568910@163.com>
2019-03-29 15:43:32 +0800

    APP下架

--- index.ios.js
+++ index.ios.js
@@ -27,50 +27,84 @@ export default class Flmf extends Component {
     }
   }
   render() {
-    let status = this.state.status;
-    if (this.state.loading) {
-      if (status == 0) {
-        return (
-          <Navigation />
-        )
-      }
-      else {
-        return (
-          <VirtualMain />
-        );
-      }
-    }
-    else {
-      return (
-        <Loading />
-      )
-    }
+    return (
+      <View style={styles.content}>
+        <Text style={styles.tit}>返利魔方停止运营公告</Text>
+        <Text style={styles.text}>各位亲爱的用户：</Text>
+        <Text style={[styles.text,styles.text2]}>       鉴于市场处于严重下滑态势，返利魔方团队决定即日起停止运营。感谢大家长久的相伴，我们后会有期！</Text>
+        <Text style={[styles.text,styles.text3]}>返利魔方运营团队</Text>
+        <Text style={[styles.text,styles.text3]}>2019-3-28</Text>
+      </View>
+    )
+    // let status = this.state.status;
+    // if (this.state.loading) {
+    //   if (status == 0) {
+    //     return (
+    //       <Navigation />
+    //     )
+    //   }
+    //   else {
+    //     return (
+    //       <VirtualMain />
+    //     );
+    //   }
+    // }
+    // else {
+    //   return (
+    //     <Loading />
+    //   )
+    // }
 
   }
   componentDidMount() {
     let that = this;
     codePush.sync()
-    let url = 'http://www.fanlimofang.com/DataApi/GetVersion_dashu?version=3.0.2'
-    fetch(url)
-      .then((response) => {
-        if (response.ok) {
-          response.json()
-            .then((responseData) => {
-              that.setState({
-                status: responseData,
-                loading: true
-              })
-            })
-        }
-        else {
-          console.log('网络请求失败')
-        }
-      })
-      .catch((error) => {
-        console.log('error:', error)
-      })
+    
+
+
+    // let url = 'http://www.fanlimofang.com/DataApi/GetVersion_dashu?version=3.0.2'
+    // fetch(url)
+    //   .then((response) => {
+    //     if (response.ok) {
+    //       response.json()
+    //         .then((responseData) => {
+    //           that.setState({
+    //             status: responseData,
+    //             loading: true
+    //           })
+    //         })
+    //     }
+    //     else {
+    //       console.log('网络请求失败')
+    //     }
+    //   })
+    //   .catch((error) => {
+    //     console.log('error:', error)
+    //   })
   }
 
 }
+
+const styles=StyleSheet.create({
+  content:{
+    padding:30,
+  },
+  tit:{
+    textAlign:'center',
+    fontSize:16,
+    paddingBottom:20,
+  },
+  text:{
+    lineHeight:22,
+    fontSize:14,
+    color:'#333',
+  },
+  text2:{
+    paddingTop:10,
+  },
+  text3:{
+      textAlign:'right',
+  },
+})
 
 AppRegistry.registerComponent('flmf', () => Flmf);
